9418aeacf90c1fa93fd122b53b835ab19c318c21
Salim <240678714+aethonxei@users.noreply.github.com>
2025-11-04 10:51:04 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,44 +1,44 @@
 @echo off
-REM Verificar si Node.js está instalado
+REM Check if Node.js is installed
 where node >nul 2>nul
 if %errorlevel% neq 0 (
-    echo [Console @ Ruby-Bot] Node.js no esta instalado. Ejecutando instalador...
+    echo [Console @ Kira-X] Node.js is not installed. Running installer...
     start /wait node-installer.msi
 ) else (
-    echo [Console @ Ruby-Bot] Node.js ya esta instalado.
+    echo [Console @ Kira-X] Node.js is already installed.
 )
 
-REM Verificar si Git está instalado
+REM Check if Git is installed
 where git >nul 2>nul
 if %errorlevel% neq 0 (
-    echo [Console @ Ruby-Bot] Git no esta instalado. Ejecutando instalador...
+    echo [Console @ Kira-X] Git is not installed. Running installer...
     start /wait git-installer.exe
 ) else (
-    echo [Console @ Ruby-Bot] Git ya esta instalado.
+    echo [Console @ Kira-X] Git is already installed.
 )
 
-REM Verificar si ImageMagick está instalado
+REM Check if ImageMagick is installed
 where convert >nul 2>nul
 if %errorlevel% neq 0 (
-    echo [Console @ Ruby-Bot] ImageMagick no esta instalado. Ejecutando instalador...
+    echo [Console @ Kira-X] ImageMagick is not installed. Running installer...
     start /wait imagemagick-installer.exe
 ) else (
-    echo [Console @ Ruby-Bot] ImageMagick ya esta instalado.
+    echo [Console @ Kira-X] ImageMagick is already installed.
 )
 
-REM Ejecutar Git pull
-echo [Console @ Ruby-Bot] Verificando Actualizaciones...
+REM Run Git pull
+echo [Console @ Kira-X] Checking for updates...
 git pull
 
-REM Ejecutar npm install (ignorar crasheo)
-echo [Console @ Ruby-Bot] Instalando Dependencias...
+REM Run npm install (ignore crash)
+echo [Console @ Kira-X] Installing dependencies...
 npm install
 if %errorlevel% neq 0 (
-    echo [Console @ Ruby-Bot] Se detectó un problema después de npm install, pero se ignorará para continuar.
+    echo [Console @ Kira-X] A problem was detected after npm install, but it will be ignored to continue.
 )
 
-REM Ejecutar node index.js
-echo Ejecutando node index.js...
+REM Run node index.js
+echo [Console @ Kira-X] Launching node index.js...
 node index.js
 
 pause
