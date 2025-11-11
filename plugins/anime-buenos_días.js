185b935ec6875724fee93cf1cb34d57cb00a00a7
Salim <240678714+aethonxei@users.noreply.github.com>
2025-11-11 11:02:59 +0100

    Update anime-buenos_días.js

--- plugins/anime-buenos_días.js
+++ plugins/anime-buenos_días.js
@@ -36,7 +36,7 @@ let handler = async (m, { conn }) => {
 
 handler.help = ['dias/days'];
 handler.tags = ['grupo'];
-handler.command = ['dias', 'dia', 'days'];
+handler.command = ['goodmorning', 'gm', 'days'];
 handler.group = true;
 
 export default handler;
